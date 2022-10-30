fcb9f5fc53d8b5d5c4fca5997a8bf4774fcd3f99
KyKhoi <kynqhe150376@fpt.edu.vn>
2022-10-31 01:00:20 +0700

    Media Manager

--- app/src/main/java/com/example/ailatrieuphu/MainActivity.java
+++ app/src/main/java/com/example/ailatrieuphu/MainActivity.java
@@ -2,9 +2,37 @@ package com.example.ailatrieuphu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.ailatrieuphu.repository.QuestionRepository;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout scoreBoard;
+    LinearLayout helper, questionLayout;
+    TextView tvQuestionId, tvQuestion, tvTimer;
+    Button button, btnA, btnB, btnC, btnD;
+    ImageButton help1, help2, help3, help4;
+    ImageView logo, rightCurtain, leftCurtain, level1, level2, level3, blur;
+
+    QuestionRepository questionRepository;
+    CountDownTimer timer1, timer2;
+
+    MediaPlayer mediaPlayer, media15;
+
+    // Player current question
+    int currentPosition = 1;
+    // Question's position in DB
+    int questionId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/ailatrieuphu/function/media/MediaManager.java
@@ -0,0 +1,82 @@
+package com.example.ailatrieuphu.function.media;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.example.ailatrieuphu.R;
+
+public class MediaManager {
+    MediaPlayer sound, backgroundMusic;
+    Context context;
+
+    public MediaManager() {
+    }
+
+    public MediaManager(Context context) {
+        this.context = context;
+    }
+
+    public void resetMedia() {
+        if (backgroundMusic != null) {
+            backgroundMusic.reset();
+        }
+    }
+
+    public void play(int number) {
+        switch (number) {
+            case 1:
+                sound = MediaPlayer.create(context, R.raw.ques_no1);
+                break;
+            case 2:
+                sound = MediaPlayer.create(context, R.raw.ques_no2);
+                break;
+            case 3:
+                sound = MediaPlayer.create(context, R.raw.ques_no3);
+                break;
+            case 4:
+                sound = MediaPlayer.create(context, R.raw.ques_no4);
+                break;
+            case 5:
+                sound = MediaPlayer.create(context, R.raw.ques_no5);
+                break;
+            case 6:
+                sound = MediaPlayer.create(context, R.raw.ques_no6);
+                break;
+            case 7:
+                sound = MediaPlayer.create(context, R.raw.ques_no7);
+                break;
+            case 8:
+                sound = MediaPlayer.create(context, R.raw.ques_no8);
+                break;
+            case 9:
+                sound = MediaPlayer.create(context, R.raw.ques_no9);
+                break;
+            case 10:
+                sound = MediaPlayer.create(context, R.raw.ques_no10);
+                break;
+            case 11:
+                sound = MediaPlayer.create(context, R.raw.ques_no11);
+                break;
+            case 12:
+                sound = MediaPlayer.create(context, R.raw.ques_no13);
+                break;
+            case 14:
+                sound = MediaPlayer.create(context, R.raw.ques_no14);
+                break;
+        }
+        sound.start();
+        if (number == 1) {
+            backgroundMusic = MediaPlayer.create(context, R.raw.phase1);
+        }
+        if (number == 6) {
+            backgroundMusic.reset();
+            backgroundMusic = MediaPlayer.create(context, R.raw.phase1);
+        }
+        if (number == 11) {
+            backgroundMusic.reset();
+            backgroundMusic = MediaPlayer.create(context, R.raw.phase1);
+        }
+        backgroundMusic.setLooping(true);
+        backgroundMusic.start();
+    }
+}
